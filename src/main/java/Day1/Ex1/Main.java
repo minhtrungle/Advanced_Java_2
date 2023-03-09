5e19093e2d163b77726540b50e5b2114e57fffad
minhtrungle <minhtrungle21599@gmail.com>
2023-03-09 13:51:39 +0700

    Update Generics

--- src/main/java/Day1/Ex1/Main.java
+++ src/main/java/Day1/Ex1/Main.java
@@ -5,5 +5,15 @@ public class Main {
         MayTinh<Double> mt = new MayTinhDouble();
         //tong
         System.out.println(mt.tong(3.0, 4.0, 5.0, 6.0));
+
+        MayTinh <PhanSo> mt1 = new MayTinhPhanSo();
+
+        PhanSo p1 = new PhanSo(1, 2);
+        PhanSo p2 = new PhanSo(3, 4);
+        PhanSo p3 = new PhanSo(5, 6);
+
+        PhanSo t1 = mt1.tong(p1, p2, p3).ruGon();
+        System.out.println(t1);
+
     }
 }
