6962d1b71e23bd0f02c0481ead5671219a2ac1c3
minhtrungle <minhtrungle21599@gmail.com>
2023-03-09 16:33:54 +0700

    Upload Generics Interface

--- src/main/java/Exercises1/Ex3/Main.java
+++ src/main/java/Exercises1/Ex3/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         st[0] = new Student(1, "LMT", 8);
         st[1] = new Student(2, "LMT2", 5);
-        st[2] = new Student(3, "LMT3", 8);
+        st[2] = new Student(3, "LMT3", 9);
 
         // Mặc định sắp xếp danh sách st theo tăng dần của điểm
         SortArray s = new SortArray();
@@ -24,5 +24,11 @@ public class Main {
         for (int i = 0; i < ht.length; i++) {
             ht[i] = new HinhTron(random.nextInt(100) + 1);
         }
+
+        //In ra 5 hình
+        s.printArray(ht);
+        //Sau khi sắp xếp
+        s.sorted(ht);
+        s.printArray(ht);
     }
 }
--- src/main/java/Exercises1/Ex3/SortArray.java
+++ src/main/java/Exercises1/Ex3/SortArray.java
@@ -15,7 +15,10 @@ public class SortArray { //Sắp xếp các đối tượng implement comparrato
 
     public <T extends MyComparator <T>> void printArray(T[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println();
+            System.out.println("Array: ");
+            for (T a : arr) {
+                System.out.println(a + "\t");
+            }
         }
     }
 }
