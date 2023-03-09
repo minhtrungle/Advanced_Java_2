e21292a31e8664e4be1c3ea0aa7c095aeb962479
minhtrungle <minhtrungle21599@gmail.com>
2023-03-09 11:02:26 +0700

    Update Day 1

---
+++ src/main/java/Day1/Ex1/MayTinh.java
@@ -0,0 +1,44 @@
+package Day1.Ex1;
+
+public class MayTinh <T> extends Number{
+    private T t1;
+    private T t2;
+
+    public T getT1() {
+        return t1;
+    }
+
+    public void setT1(T t1) {
+        this.t1 = t1;
+    }
+
+    public T getT2() {
+        return t2;
+    }
+
+    public void setT2(T t2) {
+        this.t2 = t2;
+    }
+    public tong() {
+
+    }
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+}
