cae00f240d5dd6d9c0d1621b1a16c5d19ffd4f12
minhtrungle <minhtrungle21599@gmail.com>
2023-03-09 11:31:08 +0700

    Update Generics

--- src/main/java/Day1/Ex1/Main.java
+++ src/main/java/Day1/Ex1/Main.java
@@ -1,4 +1,9 @@
 package Day1.Ex1;
 
 public class Main {
+    public static void main(String[] args) {
+        MayTinh<Double> mt = new MayTinhDouble();
+        //tong
+        System.out.println(mt.tong(3.0, 4.0, 5.0, 6.0));
+    }
 }
