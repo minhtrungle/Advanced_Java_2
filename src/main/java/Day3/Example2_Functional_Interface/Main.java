f333b0dd11bd08bf9cd6c4ecec9bb89a054b01a1
minhtrungle <minhtrungle21599@gmail.com>
2023-03-13 10:43:24 +0700

    Upload Functional Interface

---
+++ src/main/java/Day3/Example2_Functional_Interface/Main.java
@@ -0,0 +1,21 @@
+package Day3.Example2_Functional_Interface;
+
+public class Main {
+    public static void main(String[] args) {
+        MyFunctional sum = new MyFunctional() {
+            @Override
+            public double calculator(double a, double b) {
+                return a + b;
+            }
+        };
+        System.out.println(sum.calculator(3.0, 4.0));
+        //Muốn sử dụng thêm phương thức trừu tượng thì Định nghĩa 1 phương thức tính biểu thức
+        MyFunctional expression = new MyFunctional() {
+            @Override
+            public double calculator(double a, double b) {
+                return 5 * a + 3 * b;
+            }
+        };
+        System.out.println(expression.calculator(1,4));
+    }
+}
---
+++ src/main/java/Day3/Example2_Functional_Interface/MyFunctional.java
@@ -0,0 +1,7 @@
+package Day3.Example2_Functional_Interface;
+
+public interface MyFunctional {
+    //Functional Interface chỉ chứa duy nhất 1 phương thức trừu tượng
+    //Đánh dâấu Functional Interface bằng Anotation
+    double calculator(double a, double b);
+}
